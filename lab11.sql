834eb23ac2bb61618af160b5d3d48f7e7d6ad69c
israad-visuals <152644137+israad-visuals@users.noreply.github.com>
2024-11-14 23:55:24 -0500

    Updated lab11.sql
    added code

--- lab11.sql
+++ lab11.sql
@@ -31,3 +31,32 @@ WHERE status != 'Completed'
   AND course_id LIKE 'COMM%'
   AND due_date < '2024-12-31'
 ORDER BY due_date;
+
+-- This query displays the title and due_date of assignments for the course COMP1234
+SELECT title, due_date
+FROM assignments
+WHERE course_id = 'COMP1234';
+
+-- This query finds the earliest assignment due date in the assignments table
+SELECT min(due_date) AS earliest_due_date
+FROM assignments;
+
+-- This query finds the latest assignment due date in the assignments table
+SELECT max(due_date) AS latest_due_date
+FROM assignments;
+
+-- This query finds the title and course_id of assignments due on 2024-10-08
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08';
+
+-- This query displays the title and due_date of assignments due in October 2024
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%';
+
+-- This query finds the most recent due_date of assignments with a status of "Completed"
+SELECT max(due_date) AS most_recent_completed_date
+FROM assignments
+WHERE status = 'Completed';
+
